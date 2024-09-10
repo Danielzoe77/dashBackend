0a4ca07e3bf41e8fbb487d6edd0d849579ae4fa2
ehisd <omoanondan@gmail.com>
2024-09-09 23:41:49 -0300

    new rout

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,7 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const User = require("../model/userModel");
-// const protect =  asyncHandler(async (req, res, next) => {
-//   let token;
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer")
-//   ) {
-//     try {
-//       token = req.headers.authorization.split(" ")[1];
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//       req.userData = decoded;
-//       console.log("Token verified")
-//       next();
-//     } catch (error) {
-//       console.error(error);
-//       res.status(401);
-//       throw new Error("Not authorized, token failed");
-//     }
-//   }
-// })
+
 
 const protect = asyncHandler(async (req, res, next) => {
   try{
@@ -31,7 +13,7 @@ const protect = asyncHandler(async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // req.user = await User.findById(decoded.id).select("-password");
+   
     next();
   }catch(error){
     res.json({ status : false  ,message: "Not authorized, token failed"})
